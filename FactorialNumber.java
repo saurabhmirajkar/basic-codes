e21fd6b5f87aa7f4887980706ec5dad498f97b45
Saurabh <mirajkar46@gmail.com>
2018-07-17 12:33:48 +0530

    Adding Factorial code

---
+++ FactorialNumber.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class FactorialNumber {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		int number = sc.nextInt();
+		
+		// Using normal method
+		int multiple = 1;
+		for (int i = number; i > 0; i--) {
+			multiple *= i;
+		}
+		System.out.println("Factorial of " + number + " : " + multiple);
+		
+		
+		// Using recursive function
+		System.out.println("Factorial using Recursion");
+		int factorial = getFactorialOfNumber(number);
+		System.out.println("Factorial of "+ number +" is "+ factorial);
+		
+		
+		
+		sc.close();
+	}
+
+	private static int getFactorialOfNumber(int number) {
+		if(number > 1) {
+			return number * getFactorialOfNumber(number - 1);
+		}
+		else {
+			return 1;
+		}
+		
+	}
+}
